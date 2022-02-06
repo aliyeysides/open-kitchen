62301931d7f631007d2f14c63addb0716212eb5f
Ali Yeysides <ali.yeysides@gmail.com>
2022-02-06 11:48:37 -0500

    cleanup unused code

--- packages/client/src/components/ActionAreaCard.tsx
+++ packages/client/src/components/ActionAreaCard.tsx
@@ -1,13 +1,9 @@
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import CardActionArea from "@mui/material/CardActionArea";
-import CardHeader from "@mui/material/CardHeader";
-import Avatar from "@mui/material/Avatar";
-import IconButton, { IconButtonProps } from "@mui/material/IconButton";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import { red } from "@mui/material/colors";
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardHeader from '@mui/material/CardHeader';
+import Avatar from '@mui/material/Avatar';
+import { red } from '@mui/material/colors';
 
 interface ActionAreaCardProps {
   title: string;
@@ -19,27 +15,16 @@ export default function ActionAreaCard({
   thumbnail,
 }: ActionAreaCardProps) {
   return (
-    <Card variant="outlined" sx={{ maxWidth: "100%", maxHeight: "100%" }}>
+    <Card variant="outlined" sx={{ maxWidth: '100%', maxHeight: '100%' }}>
       <CardActionArea>
         <CardMedia component="img" src={thumbnail} alt="green iguana" />
-        {/* <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {title}
-          </Typography>
-        </CardContent> */}
         <CardHeader
           avatar={
             <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
               R
             </Avatar>
           }
-          // action={
-          //   <IconButton aria-label="settings">
-          //     <MoreVertIcon />
-          //   </IconButton>
-          // }
           title={title}
-          // subheader="September 14, 2016"
         />
       </CardActionArea>
     </Card>
--- packages/client/src/components/TopAppBar.tsx
+++ packages/client/src/components/TopAppBar.tsx
@@ -3,8 +3,6 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import Chip from '@mui/material/Chip';
 import LinkButton from './LinkButton';
 import KitchenIcon from '@mui/icons-material/Kitchen';
@@ -26,15 +24,6 @@ export default function TopAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
-          {/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <LinkButton to="/recipes" color="primary">
               <Box sx={{ marginRight: 1 }}>Open Kitchen </Box>

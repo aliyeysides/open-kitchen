3cb8c7d46dbfc12a316f598f5831399a3a7a51b4
Ali Yeysides <ali.yeysides@gmail.com>
2022-02-11 22:44:26 -0500

    adds tests and fix

--- packages/server/src/grocers/grocers.resolver.spec.ts
+++ packages/server/src/grocers/grocers.resolver.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { useMock } from '../../test/utils';
 import { GrocersResolver } from './grocers.resolver';
 import { GrocersService } from './grocers.service';
 
@@ -7,7 +8,7 @@ describe('GrocersResolver', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GrocersResolver, GrocersService],
+      providers: [GrocersResolver, useMock({ provide: GrocersService })],
     }).compile();
 
     resolver = module.get<GrocersResolver>(GrocersResolver);

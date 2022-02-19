6ad2b8ef9308f8aaa7d41d4c4b1bcc4ef98d6d67
Ali Yeysides <ali.yeysides@gmail.com>
2022-02-18 20:20:20 -0500

    adds tests

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 import { createUploadLink } from 'apollo-upload-client';
-import RecipePage from './pages/Recipe';
-import RecipesPage from './pages/Recipes';
-import RecipeUploadPage from './pages/RecipeUpload';
+import AppRoutes from './AppRoutes';
 
 console.log('REACT NODE ENV', process.env.NODE_ENV);
 
@@ -28,13 +25,7 @@ ReactDOM.render(
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <ApolloProvider client={client}>
-          <Routes>
-            <Route path="/" element={<App />}>
-              <Route path="recipes" element={<RecipesPage />} />
-              <Route path="recipes/:recipeId" element={<RecipePage />} />
-              <Route path="recipes/upload" element={<RecipeUploadPage />} />
-            </Route>
-          </Routes>
+          <AppRoutes />
         </ApolloProvider>
       </ThemeProvider>
     </BrowserRouter>
--- packages/client/src/setupTests.ts
+++ packages/client/src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8080';
+
+jest.mock('axios');

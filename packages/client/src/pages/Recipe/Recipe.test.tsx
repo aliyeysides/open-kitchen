d24667ffa08a34ac7f0d07b0da90b97466dfb08e
Ali Yeysides <ali.yeysides@gmail.com>
2022-02-18 20:26:13 -0500

    OK-5

--- packages/client/src/pages/Recipe/Recipe.test.tsx
+++ packages/client/src/pages/Recipe/Recipe.test.tsx
@@ -65,7 +65,7 @@ describe('Recipe Page', () => {
     );
 
     await waitFor(() => {
-      const step = screen.getByText(/Cut the cheese/i);
+      const step = screen.getByText(/cut the cheese/i);
       expect(step).toBeInTheDocument();
     });
   });

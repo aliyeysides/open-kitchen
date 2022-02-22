bdc87090ff2e42f6233087b07930267cdefe5ba0
Ali Yeysides <ali.yeysides@gmail.com>
2022-02-22 10:05:48 -0500

    refactors authprovider

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -7,31 +7,22 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 import AuthorizedApolloProvider from './components/AuthorizedApolloProvider';
-import { Auth0Provider, Auth0ProviderOptions } from '@auth0/auth0-react';
 
 import AppRoutes from './AppRoutes';
+import AuthProvider from './components/AuthProvider';
 
 console.log('REACT NODE ENV', process.env.NODE_ENV);
-
-const config: Auth0ProviderOptions = {
-  domain: `${process.env.REACT_APP_AUTH0_DOMAIN}`,
-  clientId: `${process.env.REACT_APP_AUTH0_CLIENT_ID}`,
-  audience: `${process.env.REACT_APP_AUTH0_AUDIENCE}`,
-  redirectUri: window.location.origin,
-  useRefreshTokens: true,
-  cacheLocation: 'localstorage',
-};
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Auth0Provider {...config}>
+        <AuthProvider>
           <AuthorizedApolloProvider>
             <AppRoutes />
           </AuthorizedApolloProvider>
-        </Auth0Provider>
+        </AuthProvider>
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,

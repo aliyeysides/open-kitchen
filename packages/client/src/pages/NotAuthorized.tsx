1bf3c83ece4b6b049923674b4f557c73a6f6607b
OlegZv <oleg.somov84@gmail.com>
2022-03-08 01:24:37 -0500

    rolebase page access front end
    set REACT_APP_ROLE_CLAIM_URL=https://menu-api.demo.com/roles in `.env`
    Ofc, need to find a better way to handle "not authorized" users.

---
+++ packages/client/src/pages/NotAuthorized.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/system";
+
+export default function NotAuthorized() {
+
+    return (
+        <>
+          <Box>
+            Contact Ali@gmail.com to get access!
+          </Box>
+        </>
+      );
+}

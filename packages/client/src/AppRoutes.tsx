2a61f5f04bd7ee85fdd3de8e03b337575e49803e
Ali Yeysides <ali.yeysides@gmail.com>
2022-03-23 16:39:37 -0400

    update upload route

--- packages/client/src/AppRoutes.tsx
+++ packages/client/src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import App from './App';
 import CancelPage from './pages/Checkout/Cancel';
 import SuccessPage from './pages/Checkout/Success';
+import NotFound from './pages/NotFound';
 import RecipePage from './pages/Recipe';
 import RecipesPage from './pages/Recipes';
 import TagPage from './pages/Tag';
@@ -14,6 +15,7 @@ export default function AppRoutes(): JSX.Element {
         <Route path="recipes" element={<RecipesPage />} />
         <Route path="recipes/:recipeId" element={<RecipePage />} />
         <Route path="recipes/tags/:tag" element={<TagPage />} />
+        <Route path="recipes/upload" element={<NotFound />} />
         <Route path="checkout/success" element={<SuccessPage />} />
         <Route path="checkout/cancel" element={<CancelPage />} />
       </Route>

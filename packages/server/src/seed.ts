07f23049614ca5fdc164a17f293c41d688774e4d
Ali Yeysides <ali.yeysides@gmail.com>
2022-04-21 14:23:39 -0400

    update seed

--- packages/server/src/seed.ts
+++ packages/server/src/seed.ts
@@ -99,7 +99,7 @@ const data = [
       {
         order: 2,
         instruction:
-          'Slice red onion and toss into pan. Cook onions until translucent (5-6 minutes).',
+          'Slice red onion and toss into pan with olive oil on medium-low heat. Cook onions until translucent (5-6 minutes).',
         startTime: 30,
         ingredients: [
           { name: 'red onion', quantity: 1 },
@@ -116,24 +116,18 @@ const data = [
         order: 4,
         instruction:
           'Crush and peel two cloves of garlic. Chop peeled cloves and toss into pan.',
-        startTime: 75,
+        startTime: 78,
         ingredients: [{ name: 'garlic', quantity: 2, unit: 'clove' }],
       },
       {
         order: 5,
         instruction:
-          'Turn up the heat on the pan to high and create an empty space in the center by moving contents to the edges of the pan.',
-        startTime: 90,
-      },
-      {
-        order: 6,
-        instruction:
-          'Break the sausage contents into small chunks and toss into the center of the pan. Continuously stir contents and cook the sausage until browned (2-3 minutes). ',
-        startTime: 115,
+          'Turn up the heat on the pan to high and create an empty space in the center by moving contents to the edges of the pan. Break the sausage contents into small chunks and toss into the center of the pan. Continuously stir contents and cook the sausage until browned (2-3 minutes). ',
+        startTime: 92,
         ingredients: [{ name: 'hot italian sausage', quantity: 2 }],
       },
       {
-        order: 7,
+        order: 6,
         instruction:
           'Add 1 tablespoon of paprika, 1 cup of white rice, and 1 cup of white wine. Stir thoroughly for 30 seconds before adding 2 cups of chicken stock. Reduce heat to low-medium and simmer for 15 minutes. Stir occasionally.',
         startTime: 130,
@@ -145,7 +139,7 @@ const data = [
         ],
       },
       {
-        order: 8,
+        order: 7,
         instruction:
           'Slice 3 spring onions, dice 2 tomatoes, and chop 1 bunch of fresh Italian parsley. Add all three into pan and turn heat off. Stir thoroughly and enjoy!',
         startTime: 180,
@@ -158,7 +152,7 @@ const data = [
     ],
     name: 'Spicy Sausage Rice by Gordan Ramsay',
     ytId: 'FP6E3JtmsCE',
-    tags: ['trending', 'onepot'],
+    tags: ['trending'],
   },
   {
     ingredients: [
@@ -295,292 +289,134 @@ const data = [
   {
     ingredients: [
       {
-        name: 'plum tomato',
+        name: 'Green Cabbage',
+        quantity: 1,
+        unit: 'head',
+        prod_id: 'prod_LXm1b7DIxYQSEp',
+        price_id: 'price_1KqgTgBK7YpYHqx3NPdSTo0C',
+      },
+      {
+        name: 'Red Cabbage',
+        quantity: 1,
+        unit: 'head',
+        prod_id: 'prod_LXm3Ub9WA6scyO',
+        price_id: 'price_1KqgVfBK7YpYHqx3UABfHVeP',
+      },
+      {
+        name: 'Carrot',
         quantity: 2,
-        prod_id: 'prod_LHjNIIL7HgISUo',
-        price_id: 'price_1Kb9uoBK7YpYHqx36Iyb9Lrm',
+        prod_id: 'prod_LXm6pxzbtTkkUc',
+        price_id: 'price_1KqgXwBK7YpYHqx3f8luMzqU',
       },
       {
-        name: 'garlic',
-        quantity: 2,
-        unit: 'clove',
-        prod_id: 'prod_LU62ZKzGFdnVry',
-        price_id: 'price_1Kn7qqBK7YpYHqx3zBI3Gnva',
-      },
-      {
-        name: 'hot italian sausage',
-        quantity: 2,
-        prod_id: 'prod_LU64ien3MhWkBj',
-        price_id: 'price_1Kn7skBK7YpYHqx3v8JN5WrC',
-      },
-      {
-        name: 'red bell pepper',
+        name: 'Olive Oil',
         quantity: 1,
-        prod_id: 'prod_LU66NemzI0Eyxa',
-        price_id: 'price_1Kn7uJBK7YpYHqx3lQlfDRSU',
+        prod_id: 'prod_LWzc1aRwhyrvgT',
+        price_id: 'price_1KpvdSBK7YpYHqx3sHF6DSmw',
       },
       {
-        name: 'white rice',
-        quantity: 1,
-        unit: 'cup',
-        prod_id: 'prod_LU68bDaB4y340B',
-        price_id: 'price_1Kn7vwBK7YpYHqx3DF8IZnTp',
-      },
-      {
-        name: 'red onion',
-        quantity: 1,
-        prod_id: 'prod_LU69NStMdG1SAO',
-        price_id: 'price_1Kn7xKBK7YpYHqx3aervWSzo',
-      },
-      {
-        name: 'spring onion',
-        quantity: 1,
-        unit: 'bunch',
-        prod_id: 'prod_LU6AeEIkDXTWe0',
-        price_id: 'price_1Kn7yCBK7YpYHqx3kR5Brheg',
-      },
-      {
-        name: 'italian parsley',
+        name: 'Italian Parsley',
         quantity: 1,
         unit: 'bunch',
         prod_id: 'prod_LU6CNRvnqlftVu',
         price_id: 'price_1Kn80JBK7YpYHqx3jWi62Uvt',
       },
       {
-        name: 'smoked paprika',
+        name: 'Raisins',
+        quantity: 1,
+        prod_id: 'prod_LXmDHrTvBHRIrH',
+        price_id: 'price_1KqgepBK7YpYHqx3TB4w7TCd',
+      },
+      {
+        name: 'Sliced Almonds',
+        quantity: 1,
+        prod_id: 'prod_LXmFvBScNkWgVs',
+        price_id: 'price_1Kqgh4BK7YpYHqx34LKnW6GD',
+      },
+      {
+        name: 'Honey',
+        quantity: 1,
+        prod_id: 'prod_LXmI131oqPSgZe',
+        price_id: 'price_1KqgjRBK7YpYHqx38YTEvl3c',
+      },
+      {
+        name: 'Sugar',
+        quantity: 1,
+        prod_id: 'prod_LXmJSCvX18w8hE',
+        price_id: 'price_1KqglLBK7YpYHqx355p34Bvv',
+      },
+      {
+        name: 'Red Wine Vinegar',
         quantity: 1,
         unit: 'tablespoon',
-        prod_id: 'prod_LU6EJBn3SS7jUz',
-        price_id: 'price_1Kn81ZBK7YpYHqx3KWBWAfMj',
+        prod_id: 'prod_LXmMlKM4vzkPVr',
+        price_id: 'price_1KqgnxBK7YpYHqx3c4zFIpPc',
       },
       {
-        name: 'white cooking wine',
+        name: 'Rice Vinegar',
         quantity: 1,
-        unit: 'cup',
-        prod_id: 'prod_LU6FpS8Drty2Fz',
-        price_id: 'price_1Kn82kBK7YpYHqx3oUTS77Zw',
-      },
-      {
-        name: 'chicken stock',
-        quantity: 2,
-        unit: 'cup',
-        prod_id: 'prod_LU6H7QhbQFJ1ZL',
-        price_id: 'price_1Kn84mBK7YpYHqx3ByHypTm3',
+        unit: 'tablespoon',
+        prod_id: 'prod_LXmOllhxAsN2nr',
+        price_id: 'price_1KqgpeBK7YpYHqx3ccPJ0rCb',
       },
     ],
     steps: [
       {
         order: 1,
         instruction:
-          'Prepare the sausages by piercing the casing and removing the contents. Begin heating a deep pan on low-medium heat and add a tablespoon of olive oil.',
-        startTime: 17,
-        ingredients: [{ name: 'hot italian sausage', quantity: 2 }],
+          'Start by slicing the red cabbage and add to a large bowl.',
+        startTime: 58,
+        ingredients: [{ name: 'Red Cabbage', quantity: 1 }],
       },
       {
         order: 2,
         instruction:
-          'Slice red onion and toss into pan. Cook onions until translucent (5-6 minutes).',
-        startTime: 30,
-        ingredients: [{ name: 'red onion', quantity: 1 }],
+          'Place 1 tablespoon of red wine vinegar and 1 tablespoon of rice vinegar into a sauce pan and bring to a boil. Once it starts to boil, take off the heat and pour the vinegar into the bowl while hot. Mix the contents of the bowl.',
+        startTime: 129,
+        ingredients: [
+          { name: 'Red Wine Vinegar', quantity: 1 },
+          { name: 'Rice Vinegar', quantity: 1 },
+        ],
       },
       {
         order: 3,
-        instruction: 'Dice red bell pepper and toss into pan.',
-        startTime: 60,
-        ingredients: [{ name: 'red bell pepper', quantity: 1 }],
+        instruction: 'Peel and coarsly grate both carrots and add to the bowl.',
+        startTime: 167,
+        ingredients: [{ name: 'Carrot', quantity: 2 }],
       },
       {
         order: 4,
         instruction:
-          'Crush and peel two cloves of garlic. Chop peeled cloves and toss into pan.',
-        startTime: 75,
-        ingredients: [{ name: 'garlic', quantity: 2, unit: 'clove' }],
+          'Slice the green cabbage similiar to the first step and then add the cabbage to the bowl.',
+        startTime: 189,
+        ingredients: [{ name: 'Green Cabbage', quantity: 1 }],
       },
       {
         order: 5,
         instruction:
-          'Turn up the heat on the pan to high and create an empty space in the center by moving contents to the edges of the pan.',
-        startTime: 90,
+          'Add 1/4 cup sliced almonds, 1/4 cup raisins, 1 tablespoon chopped parsley, and 4 tablespoons of olive oil. Season with a pinch of sugar, salt and pepper to the bowl.',
+        startTime: 230,
+        ingredients: [
+          { name: 'Sliced Almonds', quantity: 1 },
+          { name: 'Raisins', quantity: 1 },
+          { name: 'Italian Parsley', quantity: 1 },
+          { name: 'Olive Oil', quantity: 1 },
+          { name: 'Sugar', quantity: 1 },
+          { name: 'Salt', quantity: 1 },
+          { name: 'Pepper', quantity: 1 },
+        ],
       },
       {
         order: 6,
         instruction:
-          'Break the sausage contents into small chunks and toss into the center of the pan. Continuously stir contents and cook the sausage until browned (2-3 minutes). ',
-        startTime: 115,
-        ingredients: [{ name: 'hot italian sausage', quantity: 2 }],
-      },
-      {
-        order: 7,
-        instruction:
-          'Add 1 tablespoon of paprika, 1 cup of white rice, and 1 cup of white wine. Stir thoroughly for 30 seconds before adding 2 cups of chicken stock. Reduce heat to low-medium and simmer for 15 minutes. Stir occasionally.',
-        startTime: 130,
-        ingredients: [
-          { name: 'paprika', quantity: 1, unit: 'tablespoon' },
-          { name: 'white rice', quantity: 1, unit: 'cup' },
-          { name: 'white wine', quantity: 1, unit: 'cup' },
-          { name: 'chicken stock', quantity: 2, unit: 'cup' },
-        ],
-      },
-      {
-        order: 8,
-        instruction:
-          'Slice 3 spring onions, dice 2 tomatoes, and chop 1 bunch of fresh Italian parsley. Add all three into pan and turn heat off. Stir thoroughly and enjoy!',
-        startTime: 180,
-        ingredients: [
-          { name: 'spring onions', quantity: 1, unit: 'bunch' },
-          { name: 'italian parsley', quantity: 1, unit: 'bunch' },
-          { name: 'plum tomato', quantity: 2 },
-        ],
+          'Add 1 teaspoon of honey and mix well. Make sure to taste and adjust seasoning accordingly. Serve cold and bon appetit!',
+        startTime: 266,
+        ingredients: [{ name: 'Honey', quantity: 1 }],
       },
     ],
     name: 'How to Make the Best Cabbage Salad - Red Cabbage Salad with Apple',
     ytId: 'Fnf4TYY6Fd8',
-    tags: ['trending', 'onepot', 'vegan'],
-  },
-  {
-    ingredients: [
-      {
-        name: 'plum tomato',
-        quantity: 2,
-        prod_id: 'prod_LHjNIIL7HgISUo',
-        price_id: 'price_1Kb9uoBK7YpYHqx36Iyb9Lrm',
-      },
-      {
-        name: 'garlic',
-        quantity: 2,
-        unit: 'clove',
-        prod_id: 'prod_LU62ZKzGFdnVry',
-        price_id: 'price_1Kn7qqBK7YpYHqx3zBI3Gnva',
-      },
-      {
-        name: 'hot italian sausage',
-        quantity: 2,
-        prod_id: 'prod_LU64ien3MhWkBj',
-        price_id: 'price_1Kn7skBK7YpYHqx3v8JN5WrC',
-      },
-      {
-        name: 'red bell pepper',
-        quantity: 1,
-        prod_id: 'prod_LU66NemzI0Eyxa',
-        price_id: 'price_1Kn7uJBK7YpYHqx3lQlfDRSU',
-      },
-      {
-        name: 'white rice',
-        quantity: 1,
-        unit: 'cup',
-        prod_id: 'prod_LU68bDaB4y340B',
-        price_id: 'price_1Kn7vwBK7YpYHqx3DF8IZnTp',
-      },
-      {
-        name: 'red onion',
-        quantity: 1,
-        prod_id: 'prod_LU69NStMdG1SAO',
-        price_id: 'price_1Kn7xKBK7YpYHqx3aervWSzo',
-      },
-      {
-        name: 'spring onion',
-        quantity: 1,
-        unit: 'bunch',
-        prod_id: 'prod_LU6AeEIkDXTWe0',
-        price_id: 'price_1Kn7yCBK7YpYHqx3kR5Brheg',
-      },
-      {
-        name: 'italian parsley',
-        quantity: 1,
-        unit: 'bunch',
-        prod_id: 'prod_LU6CNRvnqlftVu',
-        price_id: 'price_1Kn80JBK7YpYHqx3jWi62Uvt',
-      },
-      {
-        name: 'smoked paprika',
-        quantity: 1,
-        unit: 'tablespoon',
-        prod_id: 'prod_LU6EJBn3SS7jUz',
-        price_id: 'price_1Kn81ZBK7YpYHqx3KWBWAfMj',
-      },
-      {
-        name: 'white cooking wine',
-        quantity: 1,
-        unit: 'cup',
-        prod_id: 'prod_LU6FpS8Drty2Fz',
-        price_id: 'price_1Kn82kBK7YpYHqx3oUTS77Zw',
-      },
-      {
-        name: 'chicken stock',
-        quantity: 2,
-        unit: 'cup',
-        prod_id: 'prod_LU6H7QhbQFJ1ZL',
-        price_id: 'price_1Kn84mBK7YpYHqx3ByHypTm3',
-      },
-    ],
-    steps: [
-      {
-        order: 1,
-        instruction:
-          'Prepare the sausages by piercing the casing and removing the contents. Begin heating a deep pan on low-medium heat and add a tablespoon of olive oil.',
-        startTime: 17,
-        ingredients: [{ name: 'hot italian sausage', quantity: 2 }],
-      },
-      {
-        order: 2,
-        instruction:
-          'Slice red onion and toss into pan. Cook onions until translucent (5-6 minutes).',
-        startTime: 30,
-        ingredients: [{ name: 'red onion', quantity: 1 }],
-      },
-      {
-        order: 3,
-        instruction: 'Dice red bell pepper and toss into pan.',
-        startTime: 60,
-        ingredients: [{ name: 'red bell pepper', quantity: 1 }],
-      },
-      {
-        order: 4,
-        instruction:
-          'Crush and peel two cloves of garlic. Chop peeled cloves and toss into pan.',
-        startTime: 75,
-        ingredients: [{ name: 'garlic', quantity: 2, unit: 'clove' }],
-      },
-      {
-        order: 5,
-        instruction:
-          'Turn up the heat on the pan to high and create an empty space in the center by moving contents to the edges of the pan.',
-        startTime: 90,
-      },
-      {
-        order: 6,
-        instruction:
-          'Break the sausage contents into small chunks and toss into the center of the pan. Continuously stir contents and cook the sausage until browned (2-3 minutes). ',
-        startTime: 115,
-        ingredients: [{ name: 'hot italian sausage', quantity: 2 }],
-      },
-      {
-        order: 7,
-        instruction:
-          'Add 1 tablespoon of paprika, 1 cup of white rice, and 1 cup of white wine. Stir thoroughly for 30 seconds before adding 2 cups of chicken stock. Reduce heat to low-medium and simmer for 15 minutes. Stir occasionally.',
-        startTime: 130,
-        ingredients: [
-          { name: 'paprika', quantity: 1, unit: 'tablespoon' },
-          { name: 'white rice', quantity: 1, unit: 'cup' },
-          { name: 'white wine', quantity: 1, unit: 'cup' },
-          { name: 'chicken stock', quantity: 2, unit: 'cup' },
-        ],
-      },
-      {
-        order: 8,
-        instruction:
-          'Slice 3 spring onions, dice 2 tomatoes, and chop 1 bunch of fresh Italian parsley. Add all three into pan and turn heat off. Stir thoroughly and enjoy!',
-        startTime: 180,
-        ingredients: [
-          { name: 'spring onions', quantity: 1, unit: 'bunch' },
-          { name: 'italian parsley', quantity: 1, unit: 'bunch' },
-          { name: 'plum tomato', quantity: 2 },
-        ],
-      },
-    ],
-    name: 'Chinese Garlic Green Beans (Chinese Restaurant Style)',
-    ytId: '5ObvT0yWtN0',
-    tags: ['trending', 'onepot', 'vegan'],
+    tags: ['trending'],
   },
 ];
 
